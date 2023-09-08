f0405258cd4763d99aabe5a26ea7f7b96399fce5
ruthomari <lynnruth02@gmail.com>
2023-09-07 17:12:19 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+#include <unistd.h>
+/**
+ * main - program prints a line
+ * Return 1 successfully 
+ */
+int main(void)
+{
+fput("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
