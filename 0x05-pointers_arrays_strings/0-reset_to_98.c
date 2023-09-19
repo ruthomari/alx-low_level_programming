94c0f9211441dd4574f7dc253ba94ecd199a9ef4
ruthomari <lynnruth02@gmail.com>
2023-09-19 09:59:44 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset n to 98
+ * @n: pointer to the value to be changed
+ * Return: void
+ */
+
+int reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void _puts(char *str);
+void puts_half(char *str);
+void puts2(char *str);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+
+#endif
