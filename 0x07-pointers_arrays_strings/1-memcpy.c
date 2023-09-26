1ef5a976fa46f03a29dd7334c7eed9f39ceeed91
* <ruthomari>
2023-09-26 23:24:28 +0300

    101-crackme_password

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -26,18 +26,16 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 
 {
+	unsigned int k;
 
-        unsigned int k;
 
+	for (k = 0; k < n; k++)
 
-        for (k = 0; k < n; k++)
+	{
 
-        {
+	dest[k] = src[k];
 
-        dest[k] = src[k];
+	}
 
-        }
-
-        return (dest);
-
+	return (dest);
 }
