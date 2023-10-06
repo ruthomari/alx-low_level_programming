d4fafa8ad7c2dff852cb69fcdf3d01e424413974
ruthomari <lynnruth02@gmail.com>
2023-10-05 19:11:25 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,26 +1,14 @@
 #include <stdlib.h>
-
 #include "main.h"
-
-
 /**
-
  * *_memset - fills memory with a constant byte
-
  * @s: memory area to be filled
-
  * @b: char to copy
-
  * @n: number of times to copy b
-
  *
-
  * Return: pointer to the memory area s
-
  */
-
 char *_memset(char *s, char b, unsigned int n)
-
 {
 unsigned int i;
 
@@ -30,23 +18,14 @@ s[i] = b;
 }
 return (s);
 }
-
 /**
-
  * *_calloc - allocates memory for an array
-
  * @nmemb: number of elements in the array
-
  * @size: size of each element
-
  *
-
  * Return: pointer to allocated memory
-
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
-
 {
 char *ptr;
 
@@ -57,10 +36,7 @@ ptr = malloc(size * nmemb);
 
 if (ptr == NULL)
 return (NULL);
-
 _memset(ptr, 0, nmemb * size);
-
 return (ptr);
-
 }
 
